640dc8067457e073aa63f0d9a17dd11b173b69cf
Varshan <varshanbb@gmail.com>
2024-09-28 16:01:41 +0800

    better kazuma

--- routes/kazuma.py
+++ routes/kazuma.py
@@ -6,28 +6,33 @@ app = Flask(__name__)
 def efficient_hunter_kazuma():
     data = request.json
     results = []
-    
+
     for hunt in data:
         monsters = hunt["monsters"]
         n = len(monsters)
         efficiency = 0
         i = 0
-        
+
         while i < n:
-            if i < n-1 and monsters[i+1] > monsters[i]:  # Look ahead for better attack timing
-                i += 1  # Move to the rear, skip this time frame
-                continue
-            
-            if monsters[i] > 0:  # Prepare to attack if there are monsters
-                if i + 1 < n and monsters[i] > monsters[i+1]:
-                    attack = monsters[i]
-                    fee = monsters[i-1] if i > 0 else 0  # Fee based on monsters at previous time
-                    efficiency += max(0, attack - fee)  # Calculate net gold gain
-                    i += 2  # Skip the cooldown time after attack
+            # Find the best monster to attack
+            best_attack = -1
+            best_index = -1
+
+            for j in range(i, min(n, i + 3)):  # Look ahead up to 3 monsters
+                if monsters[j] > best_attack:
+                    best_attack = monsters[j]
+                    best_index = j
+
+            if best_index != -1:  # We found a monster to attack
+                if best_index > 0:
+                    fee = monsters[best_index - 1]  # Fee based on previous monster
                 else:
-                    i += 1  # No attack, continue to the next frame
+                    fee = 0
+                
+                efficiency += max(0, best_attack - fee)
+                i = best_index + 1  # Move to next monster after the attack
             else:
-                i += 1  # Move to the rear if no monsters
+                i += 1  # No attack possible, move on
 
         results.append({"efficiency": efficiency})
     
