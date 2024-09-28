e4d4ef1793164ec3ca9c756ffb38083944b52cf5
Cyrus, Tan Jit Young <jityoung.tan@gmail.com>
2024-09-28 16:36:10 +0800

    update clumsy

--- clumsy_programmer.py
+++ clumsy_programmer.py
@@ -5,32 +5,28 @@ app = Flask(__name__)
 
 # Function to preprocess the dictionary by creating patterns with one letter removed
 def preprocess_dictionary(dictionary):
-    pattern_dict = defaultdict(list)
+    pattern_dict = defaultdict(set)
 
-    # Build patterns for all words in the dictionary
     for word in dictionary:
         for i in range(len(word)):
             pattern = word[:i] + '*' + word[i+1:]
-            pattern_dict[pattern].append(word)
-    
+            pattern_dict[pattern].add(word)  # Use a set for faster lookups
+
     return pattern_dict
+
+# Function to find the correct word by checking the mistyped word against the dictionary
+def find_correction(mistyped_word, pattern_dict):
+    # Generate patterns for the mistyped word and check against the pattern dictionary
+    for i in range(len(mistyped_word)):
+        pattern = mistyped_word[:i] + '*' + mistyped_word[i+1:]
+        if pattern in pattern_dict:
+            # Return the first matching word from the set
+            return next(iter(pattern_dict[pattern]))  # Get an arbitrary element from the set
+    return mistyped_word  # If no correction found, return the original word
 
 # Function to correct mistyped words using the preprocessed dictionary
 def correct_mistypes(preprocessed_dict, mistypes):
-    corrections = []
-
-    # For each mistyped word, generate patterns and check for corrections
-    for mistyped_word in mistypes:
-        found_correction = False  # Flag to indicate if a correction was found
-        for i in range(len(mistyped_word)):
-            pattern = mistyped_word[:i] + '*' + mistyped_word[i+1:]
-            if pattern in preprocessed_dict:
-                corrections.append(preprocessed_dict[pattern][0])
-                found_correction = True
-                break  # Stop checking after finding the first match
-        if not found_correction:
-            corrections.append(mistyped_word)  # Append the original if no correction is found
-
+    corrections = [find_correction(word, preprocessed_dict) for word in mistypes]
     return corrections
 
 # POST endpoint to correct the mistyped words
